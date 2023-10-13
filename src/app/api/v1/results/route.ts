4c6c2fe64611569907d1680660f78638a215d4fc
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2023-10-13 16:45:06 -0500

    fix: types

--- src/app/api/v1/results/route.ts
+++ src/app/api/v1/results/route.ts
@@ -7,6 +7,17 @@ const getValue = (row: string | null) => {
   return $.text().trim();
 }
 
+interface OmiParticipant {
+  ioi: boolean;
+  standing: string | null;
+  genre: string | null;
+  name: string | null;
+  participantId: string | null;
+  points: string | null;
+  medal: string | null;
+  school: string | null;
+}
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const contestId = searchParams.get("contestId");
@@ -19,32 +30,35 @@ export async function GET(request: Request) {
   const content = await page.content();
 
   // Get the first table in the content
-  const table = content.match(/<table.*?>([\s\S]*?)<\/table>/)[0];
+  // @ts-ignore
+  const table: string = content.match(/<table.*?>([\s\S]*?)<\/table>/)[0];
 
   // get all rows in the table that contains <span>ROO-
+  // @ts-ignore
   const rows = table.match(/<tr.*?>([\s\S]*?)<\/tr>/g).filter((row) => row.includes("<span>ROO-"));
 
-  const participants = [];
+  const participants: OmiParticipant[] = [];
 
   // map each row to an object
   rows.forEach((row, index) => {
     // split each row by td
     const splittedRow = row.match(/<td.*?>([\s\S]*?)<\/td>/g);
+    if (splittedRow) {
+      const $ = cheerio.load(row);
 
-    const $ = cheerio.load(row);
-
-    const newParticipant = {
-      ioi: splittedRow[0].includes('stats.ioinformatics.org/people/'),
-      standing: getValue(splittedRow[1]),
-      genre: getValue(splittedRow[2]),
-      name: getValue(splittedRow[3]),
-      participantId: getValue(splittedRow[4]),
-      points: getValue(splittedRow[15]),
-      medal: getValue(splittedRow[16]),
-      school: getValue(splittedRow[17]),
-    };
-
-    participants.push(newParticipant);
+      const newParticipant: OmiParticipant = {
+        ioi: Boolean(splittedRow[0].includes('stats.ioinformatics.org/people/')),
+        standing: getValue(splittedRow[1]),
+        genre: getValue(splittedRow[2]),
+        name: getValue(splittedRow[3]),
+        participantId: getValue(splittedRow[4]),
+        points: getValue(splittedRow[15]),
+        medal: getValue(splittedRow[16]),
+        school: getValue(splittedRow[17]),
+      };
+  
+      participants.push(newParticipant);
+    }   
   });
 
 
