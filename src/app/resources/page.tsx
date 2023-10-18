fffdd5ef948fbcb6547ed26005af41de5e104feb
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2023-10-17 23:20:38 -0500

    feat: update grid cols for mobile

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -84,7 +84,7 @@ const ResourcesPage = () => (
     <div className="flex flex-col gap-4 mt-2">
       <section className="">
         <h3 className="text-2xl font-bold">Plataformas recomendadas</h3>
-        <div className="grid grid-cols-3 gap-4 auto-rows-fr">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 auto-rows-fr">
           {Platforms.map((platform) => (
             <a
               key={platform.url}
@@ -107,7 +107,7 @@ const ResourcesPage = () => (
 
       <section className="">
         <h3 className="text-2xl font-bold">Libros recomendados</h3>
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {Books.map((book) => (
             <div
               key={book.referenceUrl}
